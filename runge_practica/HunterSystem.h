a1a1d587f80fb22675c99df9e27747291b59b24c
FaNoNHell <sheremet4322@gmail.com>
2024-07-14 12:03:52 +1200

    Версия с классами для множественного ввода, но с багом по T, надо найти.

---
+++ runge_practica/HunterSystem.h
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <fstream>
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+
+class HunterSystem
+{
+public:
+	void RightPartsCalc(double a, double t, double e, long double* x_and_y, long double* fx_and_fy);
+	double NoiseGen(double q, double h);
+	void SystemCalc();
+	~HunterSystem();
+	void InputData();
+private:
+	double h, t{}, T, q, a;
+	long double* x_and_y = new long double[2];
+	long double* temp1 = new long double[2];
+	long double* temp2 = new long double[2];
+	long double* temp3 = new long double[2];
+	long double* temp4 = new long double[2];
+	long double* tmp = new long double[2];
+};
+
+void HunterSystem::InputData()
+{
+	t = 0;
+	cout << "Input a: ";	//коэффициент вымирания хищников
+	cin >> a;
+	cout << "Input h: ";	//шаг
+	cin >> h;
+	cout << "Input T: ";	//промежуток времени
+	cin >> T;
+	cout << "Input q: ";	//интенсивность шума
+	cin >> q;
+	cout << "Input x: ";	//хищник
+	cin >> x_and_y[0];
+	cout << "Input y: ";	//жертва
+	cin >> x_and_y[1];
+	cout << "Данные успешно обновлены" << endl;
+}
+
+HunterSystem::~HunterSystem()
+{
+	delete[]x_and_y;
+	delete[]temp1;
+	delete[]temp2;
+	delete[]temp3;
+	delete[]temp4;
+	delete[]tmp;
+}
+
+void HunterSystem::RightPartsCalc(double a, double t, double e, long double* x_and_y, long double* fx_and_fy)
+{
+	fx_and_fy[0] = -a * (1 + e) * x_and_y[0] + x_and_y[0] * x_and_y[1];
+	fx_and_fy[1] = x_and_y[1] - x_and_y[0] * x_and_y[1];
+	return;
+}
+
+double HunterSystem::NoiseGen(double q, double h)
+{
+	srand(time(NULL));
+	double y1 = (double)rand() / RAND_MAX;	//если генерируются равномерно распределенные
+	double y2 = (double)rand() / RAND_MAX;
+	double e = q / (sqrt(h / 2)) * sqrt((-2) * log(y1)) * cos(2 * y2 * 3.1415);	//y2 точно внутри косинуса?; зачем с шагом h/2?
+	return e;
+}
+void HunterSystem::SystemCalc()
+{
+	ofstream file;
+	file.open("data.txt");
+	while (t < T)
+	{
+		double e1 = NoiseGen(q, h);	//шум в момент времени 0
+		RightPartsCalc(a, t, e1, x_and_y, temp1);
+		for (int i = 0; i < 2; i++)
+		{
+			tmp[i] = x_and_y[i] + (h * temp1[i]) / 2;
+		}
+		double e2 = NoiseGen(q, h);
+		RightPartsCalc(a, t, e2, tmp, temp2);
+		for (int i = 0; i < 2; i++)
+		{
+			tmp[i] = x_and_y[i] + (h * temp2[i]) / 2;	//зачем то округлил игрик, хотя входные данные были как надо
+		}
+
+		//шум не генерю потому что тот же момент времени?
+		RightPartsCalc(a, t, e2, tmp, temp3);
+		for (int i = 0; i < 2; i++)
+		{
+			tmp[i] = x_and_y[i] + h * temp3[i];
+		}
+
+		double e3 = NoiseGen(q, h);
+		RightPartsCalc(a, t, e3, tmp, temp4);
+
+		for (int i = 0; i < 2; i++)
+		{
+			x_and_y[i] += h * (temp1[i] + 2 * temp2[i] + 2 * temp3[i] + temp4[i]) / 6;
+		}
+		t += h;
+		cout << t << " " << x_and_y[0] << " " << x_and_y[1] << '\n';	//как выводить шум от времени? одно из значений/по формуле x и y/по другой формуле....
+		file << t << ' ' << x_and_y[0] << ' ' << x_and_y[1] << endl;
+	}
+	file.close();
+}
--- runge_practica/runge_practica.cpp
+++ runge_practica/runge_practica.cpp
@@ -1,133 +1,32 @@
-﻿#include <iostream>
-#include <conio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <fstream>
-#define _USE_MATH_DEFINES
-
-using namespace std;
-
-
-class HunterSystem
-{
-public:
-	void RightPartsCalc(double a, double t, double e, long double* x_and_y, long double* fx_and_fy);
-	HunterSystem();
-	~HunterSystem();
-	double NoiseGen(double q, double h);
-private:
-	double h, t{}, T, q, a;
-	long double* x_and_y = new long double[2];
-	long double* temp1 = new long double[2];
-	long double* temp2 = new long double[2];
-	long double* temp3 = new long double[2];
-	long double* temp4 = new long double[2];
-	long double* tmp = new long double[2];
-
-};
-
-HunterSystem::HunterSystem()
-{
-	cout << "Input a: ";	//коэффициент вымирания хищников
-	cin >> a;
-	cout << "Input h: ";	//шаг
-	cin >> h;
-	cout << "Input T: ";	//промежуток времени
-	cin >> T;
-	cout << "Input q: ";	//интенсивность шума
-	cin >> q;
-	cout << "Input x: ";	//хищник
-	cin >> x_and_y[0];
-	cout << "Input y: ";	//жертва
-	cin >> x_and_y[1];
-}
-
-HunterSystem::~HunterSystem()
-{
-	delete[]x_and_y;
-	delete[]temp1;
-	delete[]temp2;
-	delete[]temp3;
-	delete[]temp4;
-	delete[]tmp;
-}
-
-void HunterSystem::RightPartsCalc(double a, double t, double e, long double* x_and_y, long double* fx_and_fy)
-{
-	fx_and_fy[0] = -a * (1 + e) * x_and_y[0] + x_and_y[0] * x_and_y[1];
-	fx_and_fy[1] = x_and_y[1] - x_and_y[0] * x_and_y[1];
-	return;
-}
-
-double HunterSystem::NoiseGen(double q, double h)
-{
-	srand(time(NULL));
-	double y1 = (double)rand() / RAND_MAX;	//если генерируются равномерно распределенные
-	double y2 = (double)rand() / RAND_MAX;
-	double e = q / (sqrt(h / 2)) * sqrt((-2) * log(y1)) * cos(2 * M_PI * y2);	//y2 точно внутри косинуса?; зачем с шагом h/2?
-	return e;
-}
+﻿#include "HunterSystem.h"
 
 void main()
 {
-	double h, t = 0, T, q, a;
-	long double* x_and_y = new long double[2];
-	long double* temp1 = new long double[2];
-	long double* temp2 = new long double[2];
-	long double* temp3 = new long double[2];
-	long double* temp4 = new long double[2];
-	long double* tmp = new long double[2];
-	cout << "Input a: ";	//коэффициент вымирания хищников
-	cin >> a;
-	cout << "Input h: ";	//шаг
-	cin >> h;
-	cout << "Input T: ";	//промежуток времени
-	cin >> T;
-	cout << "Input q: ";	//интенсивность шума
-	cin >> q;
-	cout << "Input x: ";	//хищник
-	cin >> x_and_y[0];
-	cout << "Input y: ";	//жертва
-	cin >> x_and_y[1];
-
-	cout << t << " " << x_and_y[0] << " " << x_and_y[1] << '\n';
-	_getch();
-	ofstream file;
-	file.open("data.txt");
-	while (t < T)
+	setlocale(LC_ALL, "Russian");
+	HunterSystem HS;
+	cout << "Список команд: " << '\n' << "Нажмите 1 для введения/изменения расчетных данных;" << '\n'
+		<< "Нажмите 2 для расчета математической модели (с записью в файл результатов);" << '\n'
+		<< "Нажмите 3 (не трогать)" << '\n'
+		<< "Нажмите 4 для выхода из программы;" << '\n';
+	while (true)
 	{
-		double e1 = Noise(q, h);	//шум в момент времени 0
-		RightParts(a, t, e1, x_and_y, temp1);
-		for (int i = 0; i < 2; i++)
+		cout << "-> ";
+		int x{};
+		cin >> x;
+		switch (x)
 		{
-			tmp[i] = x_and_y[i] + (h * temp1[i]) / 2;
+		case 1:
+			HS.InputData();
+			break;
+		case 2:
+			HS.SystemCalc();
+			break;
+		case 3:
+			break;
+		case 4:
+			exit(1);
+		default:
+			break;
 		}
-		double e2 = Noise(q, h);
-		RightParts(a, t, e2, tmp, temp2);
-		for (int i = 0; i < 2; i++)
-		{
-			tmp[i] = x_and_y[i] + (h * temp2[i]) / 2;	//зачем то округлил игрик, хотя входные данные были как надо
-		}
-
-		//шум не генерю потому что тот же момент времени?
-		RightParts(a, t, e2, tmp, temp3);
-		for (int i = 0; i < 2; i++)
-		{
-			tmp[i] = x_and_y[i] + h * temp3[i];
-		}
-
-		double e3 = Noise(q, h);
-		RightParts(a, t, e3, tmp, temp4);
-
-		for (int i = 0; i < 2; i++)
-		{
-			x_and_y[i] += h * (temp1[i] + 2 * temp2[i] + 2 * temp3[i] + temp4[i]) / 6;
-		}
-		t += h;
-		cout << t << " " << x_and_y[0] << " " << x_and_y[1] << '\n';	//как выводить шум от времени? одно из значений/по формуле x и y/по другой формуле....
-		file << t << ' ' << x_and_y[0] << ' ' << x_and_y[1] << endl;
 	}
-	file.close();
-	system("pause");
 }
